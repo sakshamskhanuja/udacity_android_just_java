2f0ce3ceb283465e71cda49a7d91f1c563d5ee8e
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-03-18 21:08:22 +0530

    Added username and flavors

--- app/src/main/java/com/basic/justjava/MainActivity.java
+++ app/src/main/java/com/basic/justjava/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
     // Price of 1 coffee.
     private static final int PRICE = 414;
 
+    // Price of Whipped cream topping.
+    private static final int WHIPPED_CREAM = 82;
+
+    // Price of Chocolate topping.
+    private static final int CHOCOLATE = 165;
+
+    // Shows a short message to the user.
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +44,44 @@ public class MainActivity extends AppCompatActivity {
      * @param view is the clicked Button.
      */
     public void submitOrder(View view) {
+        // Get the entered name in "Name" EditText.
+        EditText nameEditText = findViewById(R.id.name_edit_text);
+        String name = nameEditText.getText().toString();
+
+        // Get the checked state of "Whipped cream" CheckBox.
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        boolean whippedCreamChecked = whippedCreamCheckBox.isChecked();
+
+        // Get the checked state of "Chocolate" CheckBox.
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean chocolateChecked = chocolateCheckBox.isChecked();
+
         // Store the price for the order.
-        int price = calculatePrice();
+        int price = calculatePrice(whippedCreamChecked, chocolateChecked);
 
         // Displays the price.
-        displayMessage(createOrderSummary(price));
+        displayMessage(createOrderSummary(name, price, whippedCreamChecked, chocolateChecked));
     }
 
     /**
      * Creates an order summary.
      *
-     * @param price is the total cost of the order.
+     * @param name            is the entered username in EditText.
+     * @param price           is the total cost of the order.
+     * @param addWhippedCream is true when the user has checked the "Whipped cream" Checkbox,
+     *                        otherwise, false.
+     * @param addChocolate    is true when the user has checked the "Chocolate" Checkbox, otherwise,
+     *                        false.
      * @return order summary.
      */
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(String name, int price, boolean addWhippedCream,
+                                      boolean addChocolate) {
         // Initializing Locale for India.
         Locale locale = new Locale("eng", "IN");
 
-        return "Name: John Doe" +
+        return "Name: " + name +
+                "\nAdd whipped cream? " + addWhippedCream +
+                "\nAdd chocolate? " + addChocolate +
                 "\nQuantity: " + quantity +
                 "\nTotal: " + NumberFormat.getCurrencyInstance(locale).format(price) +
                 "\nThank you!";
@@ -58,16 +90,40 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      *
+     * @param addWhippedCream is true when the user has checked the "Whipped cream" Checkbox,
+     *                        otherwise, false.
+     * @param addChocolate    is true when the user has checked the "Chocolate" Checkbox, otherwise,
+     *                        false.
      * @return price of the order.
      */
-    private int calculatePrice() {
-        return quantity * PRICE;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Checks if user has checked both the toppings.
+        if (addWhippedCream && addChocolate) {
+            return quantity * (PRICE + CHOCOLATE + WHIPPED_CREAM);
+        } else if (addWhippedCream) {
+            // Only whipped cream is checked.
+            return quantity * (PRICE + WHIPPED_CREAM);
+        } else if (addChocolate) {
+            // Only chocolate is checked.
+            return quantity * (PRICE + CHOCOLATE);
+        } else {
+            // No toppings is checked.
+            return quantity * PRICE;
+        }
     }
 
     /**
      * This method is invoked when the "+" Button is clicked. It increases the quantity by 1.
      */
     public void increment(View view) {
+        // Restricts the quantity to be 100.
+        if (quantity == 100) {
+            // Informs user that he/she cannot order less more than 100 coffee.
+            showToast(R.string.toast_hundred);
+            return;
+        }
+
+        // Increasing the quantity by 1.
         quantity++;
 
         // Updates the quantity.
@@ -75,18 +131,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is invoked when the "-" Button is clicked. It decrases the quantity by 1.
+     * This method is invoked when the "-" Button is clicked. It decreases the quantity by 1.
      */
     public void decrement(View view) {
-        quantity--;
-
-        // Restricts the quantity to be negative.
-        if (quantity < 0) {
-            quantity = 0;
+        // Restricts the quantity to be 1.
+        if (quantity == 1) {
+            // Informs user that he/she cannot order less than 1 coffee.
+            showToast(R.string.toast_one);
+            return;
         }
+
+        // Decreasing the quantity by 1.
+        quantity--;
 
         // Updates the quantity.
         displayQuantity(quantity);
+    }
+
+    /**
+     * Shows a Toast message.
+     *
+     * @param resourceID is the String resource ID.
+     */
+    private void showToast(int resourceID) {
+        // Disappears the current showing Toast.
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        // Shows the new Toast.
+        toast = Toast.makeText(this, resourceID, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     /**
